c34523bc0fa5e21884d971caeeccc86d6f16dd03
krydie01 <tech-mibs@mail.ru>
2017-12-12 23:21:16 +0900

    add owlcarouser, font-awesome. Add new section content

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,7 @@ gulp.task('js', ['common-js'], function() {
   return gulp.src([
       'app/libs/jquery/dist/jquery.min.js',
       'app/libs/wow/dist/wow.min.js',
+      'app/libs/owlcarousel/owl.carousel.min.js',
       'app/js/common.min.js', // Всегда в конце
     ])
     .pipe(concat('scripts.min.js'))
